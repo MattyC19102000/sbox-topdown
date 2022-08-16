0ca322b5198a407dbd3789bbc2087825ab4efaab
MattyC19102000 <mdcaldwell16@gmail.com>
2022-08-16 06:14:37 +0100

    Started implementing a better character controller

--- code/TopDownController.cs
+++ code/TopDownController.cs
@@ -12,12 +12,23 @@ namespace Sandbox{
 	public partial class TopDownController : BasePlayerController
 	{
 
-		[Net]
-		 protected float WalkSpeed { get; set; } = 250.0f;
+		[Net] public float Speed { get; set; } = 250.0f;
+		[Net] public float WalkSpeed { get; set; } = 150.0f;
+		[Net] public float RunSpeed { get; set; } = 400.0f;
+		[Net] public float Gravity { get; set; } = 400.0f;
 
 		public TopDownController()
 		{
 			
+		}
+
+		// Return speed based on which buttons are pressed
+		public virtual float GetSpeed()
+		{
+			if (Input.Down(InputButton.Run)) return RunSpeed;
+			if (Input.Down(InputButton.Walk)) return WalkSpeed;
+
+			return Speed;
 		}
 
 		// Make the character move
@@ -26,14 +37,23 @@ namespace Sandbox{
 			// Get current inputs to create direction vector
 			Vector3 direction = new Vector3(Input.Forward, Input.Left, 0);
 			// normalize the direction vector and multiply by walkspeed to get world space direction
-			direction = direction.Normal * WalkSpeed;
+			direction = direction.Normal * Speed;
 			// apply the movement
 			Position += direction * Time.Delta;
 		}
 
 		public override void Simulate()
 		{
+			// apply gravity
+			Velocity -= new Vector3(0, 0, Gravity) * Time.Delta;
+
 			TopDownWalk();
+
+			// if standing on the ground
+			if(GroundEntity != null) 
+			{
+				Velocity = Velocity.WithZ(0);
+			}
 		}
 
 		public override void FrameSimulate()
